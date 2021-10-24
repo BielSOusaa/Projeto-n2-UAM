f2afa2390c5b70b86b9354de2cbb598a95724419
Bruno Moreira <83652491+ProxWall@users.noreply.github.com>
2021-10-24 20:27:40 -0300

    Update projetofinal.py

--- projetofinal.py
+++ projetofinal.py
@@ -47,3 +47,40 @@ def Alterar(elemento):
             print("    Email não encontrado!      ")
 
             return
+def apagar(elemento):
+    TamanhoLista = len(lista)
+
+    for i in range(0, TamanhoLista):
+        if elemento in lista[i]:
+            print("-------------------------------------")
+            print("  Dados que seram deletados: ")
+            print(f" Nome: {lista[i][0]} \n Email:{lista[i][1]}")
+            print("-------------------------------------")
+            confirma = str(input("Confirme o delete com S ou N:")).upper()
+            if confirma == 'S':
+                del (lista[i])
+                print("-------------------------------")
+                print(" Dados deletados com sucesso! ")
+                print("-------------------------------")
+                return
+            elif confirma == 'N':
+                print("------------------------------")
+                print("     Operação cancelada!      ")
+                print("------------------------------")
+                return
+        else:
+            print("-------------------------------")
+            print("    Email não encontrado!      ")
+            print("-------------------------------")
+            return
+
+
+
+def EmOrdemAlfa():
+    TamanhoLista = len(lista)
+    listaAlfa = lista.copy()
+    listaAlfa.sort()
+    print("--------------------------------------")
+    for i in range(0, TamanhoLista):
+        print(f" Nome: {listaAlfa[i][0]} / Email:{listaAlfa[i][1]}")
+    print("-------------------------------------")
